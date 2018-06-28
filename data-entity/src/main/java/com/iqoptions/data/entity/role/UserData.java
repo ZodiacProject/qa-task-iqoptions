08f82143bc41df52ec66d8f3153e8a15e30829e0
Aleksey Stepanov <a.stepanov@propellerads.net>
2018-06-28 14:40:34 +0300

    update gitignore

---
+++ data-entity/src/main/java/com/iqoptions/data/entity/role/UserData.java
@@ -0,0 +1,20 @@
+package com.iqoptions.data.entity.role;
+
+public class UserData {
+    private final String email = "zavaxucid@99pubblicita.com";
+    private final String pass = "302bis";
+    private final String url = "https://auth.iqoption.com/api/v1.0/login";
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+
+    public String getUrl() {
+
+        return url;
+    }
+}
